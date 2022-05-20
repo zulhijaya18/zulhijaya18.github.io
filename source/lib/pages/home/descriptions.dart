e7101f72d9166809636c1e58336432b060f8dae5
zulhijaya18 <32.zulhijaya@gmail.com>
2022-05-20 22:44:27 +0800

    update

--- source/lib/pages/home/descriptions.dart
+++ source/lib/pages/home/descriptions.dart
@@ -1,11 +1,6 @@
 import 'dart:html' as html;
-import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:zulhijaya_profile/services/game_catalogue.dart';
 
 class Description extends StatelessWidget {
   const Description({Key? key}) : super(key: key);
@@ -23,18 +18,22 @@ class Description extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20),
-        // GestureDetector(onTap: () {}),
         const Text(
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Saya seorang mahasiswa tingkat akhir Program Studi Pendidikan Teknik Informatika dan Komputer di Universitas Negeri Makassar (UNM). Saya mengambil konsentrasi Rekayasa Perangkat Lunak (RPL). Saat ini fokus saya adalah mengembangkan potensi saya sebagai seorang Mobile App Developer. Programming language yang saya gunakan adalah Dart dipadu dengan Flutter. Selain Dart/Flutter, saya juga mahir menggunakan React Native dan Ionic Framework. ",
           textAlign: TextAlign.justify,
           style: TextStyle(fontSize: 14),
         ),
-        const SizedBox(height: 40),
+        const SizedBox(height: 20),
         Row(children: [
           ...[
             {'label': 'GitHub', 'url': 'https://github.com/zulhijaya18'},
             {'label': 'WakaTime', 'url': 'https://wakatime.com/@Zulhijaya'},
             {'label': 'Instagram', 'url': 'https://instagram.com/zul_hijaya'},
+            {
+              'label': 'LinkedIn',
+              'url':
+                  'https://www.linkedin.com/in/zulhijaya-zulhijaya-07135b229/'
+            },
           ]
               .map(
                 (e) => Container(
@@ -48,14 +47,6 @@ class Description extends StatelessWidget {
                 ),
               )
               .toList(),
-          InkWell(
-            onTap: () {
-              int index = Random().nextInt(games.length);
-              html.window.open(games[index]['url'], "_self");
-              // Get.toNamed('/game');
-            },
-            child: const Text('Play Game?'),
-          ),
         ]),
       ],
     );
